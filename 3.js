34d289975d1919ebf2b0162c3f8cef7dc514f127
Lnkeyd <the.lnkeyd@gmail.com>
2023-10-18 23:08:16 +0300

    3 has done

--- 3.js
+++ 3.js
@@ -6,49 +6,50 @@
 
 // Будет плюсом, если задумаетесь и об оптимизации.
 
-const MathX = {
+const MathX = () => {
   // вычисление N-го числа в ряду Фибоначчи Ф
-  fibN(n) {
+  const fibN = (n) => {
     if (n < 0) return -1;
     if (n <= 1) return n;
 
-    return this.fibN(n - 1) + this.fibN(n - 2);
-  },
+    return fibN(n - 1) + fibN(n - 2);
+  };
 
-  fibAllN(n) {
+  // вычисление всех чисел в ряду Фибоначчи до числа N
+  const fibAllN = (n) => {
     const ans = [];
     for (let i = 0; i < n; i++) {
-      ans.push(this.fibN(i));
+      ans.push(fibN(i));
     }
     return ans.toString();
-  },
+  };
 
   //   вспомагательная ф-ция для проверки на простое число
-  isPrime(n) {
-    for (let i = 2; i < n; i++) {
+  const isPrime = (n) => {
+    for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
       if (n % i === 0) return false;
     }
     return n !== 1;
-  },
+  };
 
   // вычисление всех простых чисел до числа N
-  primeAllN(n) {
+  const primeAllN = (n) => {
     const ans = [];
     for (let i = 2; i < n; i++) {
-      if (this.isPrime(i)) ans.push(i);
+      if (isPrime(i)) ans.push(i);
     }
 
     return ans.toString();
-  },
+  };
 
   // вычисление N-го просто числа
-  primeN(n) {
+  const primeN = (n) => {
     const ans = [];
     let count = 0;
     let i = 1;
 
     while (count < n) {
-      if (this.isPrime(i)) {
+      if (isPrime(i)) {
         ans.push(i);
         count++;
       }
@@ -56,5 +57,15 @@ const MathX = {
     }
 
     return ans.pop();
-  },
+  };
+
+  return {
+    fibN,
+    fibAllN,
+    isPrime,
+    primeAllN,
+    primeN,
+  };
 };
+
+const myMath = MathX();
